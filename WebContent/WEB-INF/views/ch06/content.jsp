b93b900f143c3a1e2b9533b6ef2812e3f975dfaa
user4 <user4@iot.com>
2019-10-10 19:34:31 +0900

    수정

--- WebContent/WEB-INF/views/ch06/content.jsp
+++ WebContent/WEB-INF/views/ch06/content.jsp
@@ -20,10 +20,42 @@
 			function btnLogout(){
 				location.href = "logout";
 			}
+			function jsonDownload1(){
+				$.ajax({
+					url:"jsonDownload1",
+					success:function(data){						
+						var html = "";
+						html +=	"<tr>";
+						html += 	"<td>"+ data.bno +"</td>";
+						html += 	"<td>"+ data.btitle +"</td>";
+						html += 	"<td>"+ data.writer +"</td>";
+						html += 	"<td>"+ data.date +"</td>";
+						html += 	"<td>"+ data.hitcount +"</td>";
+						html +=	"<tr>";
+						$("tbody").append(html);
+					}
+				});
+			}
+			function jsonDownload2(){
+				$.ajax({
+					url:"jsonDownload2",
+					success:function(data){						
+						var html = "";
+						html +=	"<tr>";
+						html += 	"<td>"+ data.bno +"</td>";
+						html += 	"<td>"+ data.btitle +"</td>";
+						html += 	"<td>"+ data.writer +"</td>";
+						html += 	"<td>"+ data.date +"</td>";
+						html += 	"<td>"+ data.hitcount +"</td>";
+						html +=	"<tr>";
+						$("tbody").append(html);
+					}
+				});
+			}
 		</script>
 	</head>
 	<body>
-		<h5>[content.jsp]</h5>
+		<h5>[HttpSession을 이용해서 로그인 구현]</h5>
 		<div>
 			<c:if test="${loginResult != 'success'}"><%-- test값이 true이면 실행 --%>
 				<form id="loginForm" method="post" action="login">
@@ -50,6 +82,35 @@
 					<button onclick="btnLogout()" class="btn btn-danger">로그아웃</button>
 				</div>
 			</c:if>
+		</div>
+		<h5>[OutputStream을 이용해서 파일 다운로드 구현]</h5>
+		<div>
+			<img src="<%=application.getContextPath()%>/resources/image/Chrysanthemum.jpg" width="100"> <!-- 고정적인 이미지 -->
+			<br/>  
+			<img src="fileDownload?fname=Chrysanthemum.jpg" width="100"/> 
+			<!-- 컨트롤러가 실행 // 조건에 따라 사용하는 이미지 다를 떄 사용 -->
+			<br/>	
+			<a href="fileDownload?fname=Chrysanthemum.jpg">파일 다운로드</a>
+			<br/>
+		</div>
+		<br/>
+		<h5>[Writer를 이용해서 JSON 데이터 다운로드]</h5>
+		<a href="javascript:jsonDownload1()">JSP에서 생성</a><br/>
+		<a href="javascript:jsonDownload2()">Controller에서 생성</a><br/>
+		<div>
+			<table class="table table-sm">
+			  <thead>
+			    <tr>
+			      <th scope="col">번호</th>
+			      <th scope="col">제목</th>
+			      <th scope="col">글쓴이</th>
+			      <th scope="col">날짜</th>
+			      <th scope="col">조회수</th>
+			    </tr>
+			  </thead>
+			  <tbody>
+			  </tbody>
+			</table>
 		</div>
 	</body>
 </html>
--- src/main/java/com/mycompany/web/controller/Ch06Controller.java
+++ src/main/java/com/mycompany/web/controller/Ch06Controller.java
@@ -1,14 +1,35 @@
 package com.mycompany.web.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mycompany.web.dto.Ch06Board;
+
 
 @Controller
 @RequestMapping("/ch06")
 public class Ch06Controller {
+	private static final Logger logger = LoggerFactory.getLogger(Ch06Controller.class);
 	@RequestMapping("/content")
 	public String content() {
 		return "ch06/content";
@@ -36,5 +57,83 @@ public class Ch06Controller {
 	public String logout(HttpSession session) {
 		session.removeAttribute("loginResult");
 		return "redirect:/ch06/content"; 
+	}
+	
+	@RequestMapping("/fileDownload")
+	public void fileDownload(String fname, HttpServletResponse response, HttpServletRequest request) throws Exception {
+		logger.debug(fname);
+		//응답 헤더에 추가
+		ServletContext application = request.getServletContext();
+		String mimeType = application.getMimeType(fname);
+		response.setHeader("Content-Type", mimeType); //response.setContentType("image/png");
+		
+		String downloadName;
+		String userAgent = request.getHeader("User-Agent");
+		if(userAgent.contains("Trident/7.0") || userAgent.contains("MSIE")) {
+			//IE11 버전 또는 IE10 이하 버전에서 한글 파일을 복원하기 위해 
+			downloadName = URLEncoder.encode(fname, "UTF-8");
+		}else {
+			//Webkit 기반 브라우저(Chrome, Safari, FireFox, Opera, Edge)에서 한글 파일을 복원하기 위해
+			downloadName = new String(fname.getBytes("UTF-8"),"ISO-8859-1");
+		}
+		response.setHeader("Content-Disposition", "attachment; filename=\""+downloadName+"\"");
+		
+		String realPath = application.getRealPath("/resources/image/" +fname);
+		File file = new File(realPath);
+		response.setHeader("Content-Length", String.valueOf(file.length()));
+		
+		//응답 본문에 데이터 추가
+		OutputStream os = response.getOutputStream();
+		InputStream is = new FileInputStream(realPath);
+		
+		byte[] buffer = new byte[1024];
+		while(true) {
+			int readByte = is.read(buffer);
+			if(readByte == -1) break;
+			os.write(buffer,0,readByte);
+		}
+		os.flush();
+		os.close();
+		is.close();
+	}
+	
+	@RequestMapping("/jsonDownload1")
+	public String jsonDownload1(Model model) {
+		Ch06Board board = new Ch06Board();
+		board.setBno(100);
+		board.setBtitle("공부하고 싶다.");
+		board.setBcontent("까짓것 하면 되겠지...열공!");
+		board.setWriter("감못잡아");
+		board.setDate(new Date());
+		board.setHitcount(1);		
+		model.addAttribute("board",board);
+		
+		return "ch06/jsonDownload1";
+	}
+	
+	@RequestMapping("/jsonDownload2")
+	public void jsonDownload2(HttpServletResponse response) throws Exception {
+		Ch06Board board = new Ch06Board();
+		board.setBno(300);
+		board.setBtitle("나는 오타쟁이");
+		board.setBcontent("오타는 나의 인생, 오타 내는 것은 당연, 근데 못찾으면 안됨!");
+		board.setWriter("감잡아");
+		board.setDate(new Date());
+		board.setHitcount(1);	
+		
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("bno", board.getBno());
+		jsonObject.put("btitle", board.getBtitle());
+		jsonObject.put("writer", board.getWriter());
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		jsonObject.put("date", sdf.format(board.getDate()));
+		jsonObject.put("hitcount", board.getHitcount());
+		String json = jsonObject.toString();
+		
+		response.setContentType("application/json; charset=UTF-8");
+		PrintWriter pw = response.getWriter();
+		pw.write(json);
+		pw.flush();
+		pw.close();
 	}
 }
